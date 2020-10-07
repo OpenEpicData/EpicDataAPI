b43835f87626991e64f3a53d986c1313fff188b4
AGDholo <agdholo@gmail.com>
2020-10-07 23:00:17 +0800

    chore: add deploy Q A

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 # CD Q & A: https://stackoverflow.com/questions/29045140/env-bash-r-no-such-file-or-directory/53943650
 
+cd ../
 yarn build --production
+
 cd ./build
 yarn install --production
 pm2 restart server.js
